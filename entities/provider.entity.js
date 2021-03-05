76ce79408912dba166c1178ad83373a393dec99e
Mostafa Hisham <mmham3919@gmail.com>
2021-03-05 08:34:48 +0200

    adding provider entity

---
+++ entities/provider.entity.js
@@ -0,0 +1,29 @@
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const Product = require('./product.entity');
+const sequelize = new Sequelize('sqlite::memory');
+
+class Provider extends Model {}
+
+Provider.init({
+  // Model attributes are defined here
+  name: {
+    type: DataTypes.STRING(45),
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW    
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW   
+  },
+}, {
+  // Other model options go here
+  sequelize, // We need to pass the connection instance
+  modelName: 'provider' // We need to choose the model name
+});
+
+Provider.belongsToMany(Product, { through: 'product_providers' });
+
+module.exports = Provider;
