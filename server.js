5705c49243c0241f263bbd9814b6fa7a11377428
Mostafa Hisham <mmham3919@gmail.com>
2021-03-05 04:58:08 +0200

    adding scripts to run app

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+// Routes
+// const routes = require('./routes/index');
+
+// Load env vars
+dotenv.config({ path: '.env' });
+
+const app = express();
+
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+   app.use(
+      morgan((tokens, req, res) =>
+         [
+            `<${process.env.NODE_ENV}>`,
+            tokens.method(req, res),
+            tokens.url(req, res),
+            tokens.status(req, res),
+            tokens.res(req, res, 'content-length'),
+            '-',
+            tokens['response-time'](req, res),
+            'ms',
+         ].join(' '),
+      ),
+   );
+}
+
+// Mount the routes
+// app.use(routes);
+
+// Port
+const port = process.env.PORT || '3000';
+
+// Listen
+const server = app.listen(port, () =>
+   console.log(`App Listen Successfully To Port ${port}`),
+);
+
+// Unhandled Promise Rejection Handler
+process.on('unhandledRejection', ex => {
+   console.error(`${ex.message}`, ex);
+   app.use((_req, res) => {
+      res.status(500).json({
+         success: false,
+         msg: '500 Internet Error',
+         data: null,
+      });
+   });
+
+   server.close(() => process.exit(1));
+});
