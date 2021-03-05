506f153fa3cc722833b6964d0077b0e11aad6f71
Mostafa Hisham <mmham3919@gmail.com>
2021-03-05 09:25:10 +0200

    fix db configuration

--- config/db.js
+++ config/db.js
@@ -1,26 +1,22 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
+const init = () => {
+    return new Sequelize({
         dialect: 'mysql',
         host: process.env.DATABASE_HOST,
-        port: process.env.DATABASE_PORT,
+        port: +process.env.DATABASE_PORT,
         username: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_PASWORD,
+        password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_DATABASENAME,
         logging: (query) => {
           console.log('query', { query });
         },
-        dialectOptions: {
-          decimalNumbers: true,
-          options: {
-            enableArithAbort: true,
-            encrypt: true,
-          },
-        },
-});
+   });
+}
 
-const connectDB = async (sequelize) => {
+const connectDB = async () => {
   try {
+    const sequelize = init()
     await sequelize.authenticate();
     
      console.log('Connection has been established successfully.');
@@ -30,6 +26,6 @@ const connectDB = async (sequelize) => {
 }
 
 module.exports = {
-    sequelize,
+    init,
     connectDB
 }
