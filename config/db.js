4df1f3afbe871c3429d0d738c0823e14d7a43dda
Mostafa Hisham <mmham3919@gmail.com>
2021-03-05 16:20:03 +0200

    fix config db file

--- config/db.js
+++ config/db.js
@@ -1,31 +1,31 @@
 const { Sequelize } = require('sequelize');
-
-const init = () => {
-    return new Sequelize({
-        dialect: 'mysql',
-        host: process.env.DATABASE_HOST,
-        port: +process.env.DATABASE_PORT,
-        username: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_DATABASENAME,
-        logging: (query) => {
-          console.log('query', { query });
-        },
-   });
-}
+const initializeEntities = require('./../entities');
 
 const connectDB = async () => {
   try {
-    const sequelize = init()
+    const sequelize = new Sequelize({
+      dialect: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASENAME,
+      logging: (query) => {
+        console.log('query', { query });
+      },
+    });
+  
     await sequelize.authenticate();
-    
-     console.log('Connection has been established successfully.');
+  
+    // Uncomment this following statements while initialize models
+    // const entities = initializeEntities(sequelize);
+    // await entities.sequelize.sync();
+    console.log('Connection has been established successfully.');
     } catch (err) {
      console.error('Unable to connect to the database:', err);
     }
 }
 
 module.exports = {
-    init,
     connectDB
 }
