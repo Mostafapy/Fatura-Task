179ae164d99ce9999232ff7771f4e2f6e93302fe
Mostafa Hisham <mmham3919@gmail.com>
2021-03-05 08:21:26 +0200

    adding product entity

---
+++ entities/product.entity.js
@@ -0,0 +1,42 @@
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const Category = require('./category.entity');
+const Provider = require('./provider.entity');
+const sequelize = new Sequelize('sqlite::memory');
+
+class Product extends Model {}
+
+Product.init({
+  // Model attributes are defined here
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW    
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW   
+  },
+  productId: {
+    type: DataTypes.INTEGER,
+    references: {
+     model: 'product', // 'category' refers to table name
+     key: 'id', // 'id' refers to column name in category table
+    }
+  },
+  providerId: {
+    type: DataTypes.INTEGER,
+    references: {
+     model: 'product', // 'category' refers to table name
+     key: 'id', // 'id' refers to column name in category table
+    }
+  }
+}, {
+  // Other model options go here
+  sequelize, // We need to pass the connection instance
+  modelName: 'product' // We need to choose the model name
+});
+
+Product.belongsTo(Category);
+
+Product.belongsToMany(Provider, { through: 'product_providers' });
+
+module.exports = Product;
