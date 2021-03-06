f106f894ac9d75267b0171c2941dcb9319f09c9a
Mostafa Hisham <mmham3919@gmail.com>
2021-03-06 13:16:57 +0200

    fix pagination middleware

--- common/middleware/pagination.js
+++ common/middleware/pagination.js
@@ -4,8 +4,15 @@ module.exports = model => {
       const limit = 25;
       pageNumber -= 1;
   
-      const data = await model.findAll();
-      
+      const conditions = {};
+      if (Object.keys(req.query).length > 1) {
+        const keys = Object.keys(req.query).filter(key => key != 'page');
+        keys.forEach(key => {
+            conditions[key] = req.query[key];
+        })
+      }
+      const data = await model.findAll(conditions);
+
       const result  = data.slice(page * limit, (page + 1) * limit);
  
       res.paginatedResult = {
