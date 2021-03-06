f797807e1cf8cce7978101acab00536b5eb7c2f2
Mostafa Hisham <mmham3919@gmail.com>
2021-03-06 21:38:19 +0200

    adding update category controller

---
+++ modules/category/controllers/updateCategory.js
@@ -0,0 +1,26 @@
+const { OK } = require('http-status-codes');
+
+const asyncHandler = require('../../../common/middleware/async');
+const ErrorResponse = require('../../../common/utils/errorResponse');
+const Category = require('../../../entities/category.entity');
+
+// @desc      update category
+// @route     PUT /api/v0/category/update/:id
+// @access    Public
+module.exports = asyncHandler(async (req, res, next) => {
+ 
+  const { id } = req.params;
+
+  const category = await Category.findOne({ where: { id } });
+
+  if (!category) {
+    return next(new ErrorResponse('No Category found', NOT_FOUND));
+  }
+  // update category
+  const category = await Category.update(req.body, { where: { id }});
+
+  const category = await Category.findOne({ where: { id } });
+  return res
+    .status(OK)
+    .json({ status: true, message: 'Provider Updated', data: category });
+});
