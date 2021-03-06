de9722a53ea8fb97b4e5a83f5b84bca320527fca
Mostafa Hisham <mmham3919@gmail.com>
2021-03-06 21:38:51 +0200

    add update product controller

---
+++ modules/product/controllers/updateProduct.js
@@ -0,0 +1,26 @@
+const { OK } = require('http-status-codes');
+
+const asyncHandler = require('../../../common/middleware/async');
+const ErrorResponse = require('../../../common/utils/errorResponse');
+const Product = require('../../../entities/product.entity');
+
+// @desc      update product
+// @route     PUT /api/v0/product/update/:id
+// @access    Public
+module.exports = asyncHandler(async (req, res, next) => {
+ 
+  const { id } = req.params;
+
+  const product = await Product.findOne({ where: { id } });
+
+  if (!product) {
+    return next(new ErrorResponse('No Product found', NOT_FOUND));
+  }
+  // update product
+  const product = await Product.update(req.body, { where: { id }});;
+
+  const product = await Product.findOne({ where: { id } });
+  return res
+    .status(OK)
+    .json({ status: true, message: 'Provider Updated', data: product });
+});
