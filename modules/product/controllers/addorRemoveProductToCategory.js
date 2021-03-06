4e1a7d70f338e928e0b4f5ba822d730c179b7d91
Mostafa Hisham <mmham3919@gmail.com>
2021-03-06 21:52:22 +0200

    add or remove product to category controller

---
+++ modules/product/controllers/addorRemoveProductToCategory.js
@@ -0,0 +1,34 @@
+const { NOT_FOUND } = require("http-status-codes");
+const ErrorResponse = require("../../../common/utils/errorResponse");
+const Category = require("../../../entities/category.entity");
+const Product = require("../../../entities/product.entity");
+
+// @desc      add or product category
+// @route     PUT  /api/v0/product/:id/category/:categoryId
+// @access    Public
+module.exports = asyncHandler(async (req, res, next) => {
+     const { id, categoryId } =  req.params;
+     const product = await Product.findOne({ where: { id }});
+
+     if (!product) {
+        return next(new ErrorResponse('No product found', NOT_FOUND));
+     }
+
+     const category = await Category.findOne({ where: { id: categoryId }});
+
+     if (!Category) {
+        return next(new ErrorResponse('No category found', NOT_FOUND));
+     }
+     if (product.categoryId && product.categoryId == category.id) {
+         product.categoryId = null
+     } else if (!product.categoryId) {
+         await Product.update({ categoryId }, {where: { id: product.id }});
+     } else if (product.categoryId != category.id) {
+        return next(new ErrorResponse('No Product found', NOT_FOUND));
+     }
+
+      // Get product by id
+      const product = await Product.findOne({ where: { id } });
+
+      return product;
+});
