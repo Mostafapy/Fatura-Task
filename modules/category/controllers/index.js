7b9758895eb5c259e2770581e83b50fc447c34cd
Mostafa Hisham <mmham3919@gmail.com>
2021-03-06 22:23:15 +0200

    adding index file for all routes in category module

---
+++ modules/category/controllers/index.js
@@ -0,0 +1,11 @@
+const addCategoryController = require('./addCategory');
+const getAllCategoriesController = require('./getAllCategories');
+const getCategoryByIdController = require('./getCategoryById');
+const updateCategoryController = require('../controllers/updateCategory');
+
+module.exports =  {
+    addCategoryController,
+    getAllCategoriesController,
+    getCategoryByIdController,
+    updateCategoryController
+}
