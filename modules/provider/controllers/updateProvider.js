0dffd306ff0d6ebba7d6df83fb11ef620d841668
Mostafa Hisham <mmham3919@gmail.com>
2021-03-06 21:45:00 +0200

    add update provider controller

---
+++ modules/provider/controllers/updateProvider.js
@@ -0,0 +1,26 @@
+const { OK } = require('http-status-codes');
+
+const asyncHandler = require('../../../common/middleware/async');
+const ErrorResponse = require('../../../common/utils/errorResponse');
+const Provider = require('../../../entities/provider.entity');
+
+// @desc      update provider
+// @route     PUT /api/v0/provider/update/:id
+// @access    Public
+module.exports = asyncHandler(async (req, res, next) => {
+ 
+  const { id } = req.params;
+
+  const provider = await Provider.findOne({ where: { id } });
+
+  if (!provider) {
+    return next(new ErrorResponse('No Provider found', NOT_FOUND));
+  }
+  // update provider
+  const provider = await Provider.update(req.body, { where: { id }});
+
+  const provider = await Provider.findOne({ where: { id } });
+  return res
+    .status(OK)
+    .json({ status: true, message: 'Provider Updated', data: provider });
+});
